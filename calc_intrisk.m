3554cc65572fc6e438028bf0579ad5c8ca66c05b
Nick Magliocca <nmagliocca@sesync.org>
2017-02-06 14:38:41 -0500

    Fixed time discounting bug in 'calc_intrisk'.

--- calc_intrisk.m
+++ calc_intrisk.m
@@ -3,9 +3,9 @@ function [sl_risk,intrd_risk,slevnt,intrdevnt,tmevnt]=calc_intrisk(sloccur,...
 
 % slevnt=sum(sum(sloccur,1).*timeweight.^(t-1:-1:TSTART));
 % intrdevnt=sum(sum(intrdoccur,2)'.*timeweight.^(t-1:-1:TSTART));
-slevnt=sum(sloccur.*repmat((timeweight.^(t-1:-1:TSTART))',1,length(sloccur(1,:))),1);
-intrdevnt=sum(intrdoccur.*repmat(timeweight.^(t-1:-1:TSTART),length(intrdoccur(:,1)),1),2);
-tmevnt=sum(timeweight.^(t-1:-1:TSTART));
+slevnt=sum(sloccur.*repmat((timeweight.^(t-(t:-1:TSTART+1)))',1,length(sloccur(1,:))),1);
+intrdevnt=sum(intrdoccur.*repmat(timeweight.^(t-(t:-1:TSTART+1)),length(intrdoccur(:,1)),1),2);
+tmevnt=sum(timeweight.^(t-(t:-1:TSTART+1)));
 sl_risk=(slevnt+alpharisk)./(tmevnt+alpharisk+betarisk);
 intrd_risk=(intrdevnt+alpharisk)./(tmevnt+alpharisk+betarisk);
 end
